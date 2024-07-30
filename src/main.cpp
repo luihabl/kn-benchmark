4bf519d5dba4c63af469fb3eeb59b4b55fc9e0b1
Lui Habl <txaicalvoso@gmail.com>
2024-07-29 21:17:58 -0300

    Add cs data

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,28 @@
 #include <cstdio>
+#include "kn/collisions/mcc.h"
+
 
 int main() {
+
+	double f = 13.56e6;
+	double dt = 1.0 / (400.0 * f);
+	double l = 6.7e-2;
+	double dx = l / 128.0;
+	double ng = 9.64e20;
+	double tg = 300.0;
+	
+
+	kn::collisions::MonteCarloCollisions::DomainConfig coll_config;
+	coll_config.m_dt = dt;
+	coll_config.m_m_dx = dx;
+	coll_config.m_n_neutral = ng;
+	coll_config.m_t_neutral = tg;
+
+
+	
+
+
+	
 	printf("hello!\\n");
 	return 0;
 }
